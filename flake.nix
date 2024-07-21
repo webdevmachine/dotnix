4ea165548156db965346dc658849bd4df30c0425
webdevmachine <thewebdevmachine@gmail.com>
2024-07-21 16:59:11 +0200

    feat(configurations/flake): initialize dotnix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  outputs =
+    inputs@{ flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      imports = [ ./configurations/flake ];
+    };
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    systems = {
+      url = "github:nix-systems/default";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+}
