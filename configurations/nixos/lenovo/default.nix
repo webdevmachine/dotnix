c511e649ec66259870fa0846cad687074f185dcd
webdevmachine <thewebdevmachine@gmail.com>
2024-07-23 15:22:20 +0200

    feat(configurations/nixos): initialize lenovo system

---
+++ configurations/nixos/lenovo/default.nix
@@ -0,0 +1,5 @@
+{
+  networking.hostName = "lenovo";
+  nixpkgs.hostPlatform = "x86_64-linux";
+  system.stateVersion = "24.11";
+}
--- profiles/flake/configurations.nix
+++ profiles/flake/configurations.nix
@@ -1,6 +1,21 @@
+{ self, inputs, ... }:
+let
+  inherit (inputs.nixpkgs.lib) nixosSystem;
+
+  nixosConfiguration =
+    { configuration }:
+    nixosSystem {
+      modules = [ configuration ];
+      specialArgs = {
+        inherit self inputs;
+      };
+    };
+in
 {
   flake = {
-    nixosConfigurations = { };
+    nixosConfigurations = {
+      lenovo = nixosConfiguration { configuration = ../../configurations/nixos/lenovo; };
+    };
     darwinConfigurations = { };
     homeConfigurations = { };
   };
