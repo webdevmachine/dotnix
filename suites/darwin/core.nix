c64f63c5d78fd978611c12bb60a9b482c7f64acd
webdevmachine <thewebdevmachine@gmail.com>
2024-08-25 12:42:00 +0200

    feat(suites/darwin): configure core darwin suite

---
+++ suites/darwin/core.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ../../profiles/common/nix/arguments.nix
+    ../../profiles/common/nix/settings.nix
+    ../../profiles/darwin/nix/settings.nix
+
+    ../../profiles/system/users/core.nix
+    ../../profiles/darwin/users/core.nix
+  ];
+}
--- systems/workbook/default.nix
+++ systems/workbook/default.nix
@@ -1,12 +1,5 @@
 {
-  imports = [
-    ../../profiles/common/nix/arguments.nix
-    ../../profiles/common/nix/settings.nix
-    ../../profiles/darwin/nix/settings.nix
-
-    ../../profiles/system/users/core.nix
-    ../../profiles/darwin/users/core.nix
-  ];
+  imports = [ ../../suites/darwin/core.nix ];
 
   networking.hostName = "NS-WRF00K0P79";
   nixpkgs.hostPlatform = "aarch64-darwin";
