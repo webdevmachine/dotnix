c22a716b608d0ae0af514883cab349ea9b356c6a
webdevmachine <thewebdevmachine@gmail.com>
2024-08-25 10:36:49 +0200

    feat(profiles/flake): configure nix-specific formatters

--- profiles/flake/formatter.nix
+++ profiles/flake/formatter.nix
@@ -10,6 +10,16 @@
         package = pkgs.treefmt2;
         programs = {
           prettier.enable = true;
+
+          deadnix.enable = true;
+          statix.enable = true;
+          nixfmt.enable = true;
+        };
+
+        settings.formatter = {
+          deadnix.priority = 1;
+          statix.priority = 2;
+          nixfmt.priority = 3;
         };
       };
 
